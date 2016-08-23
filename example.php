bdb304419ce2af806af5995e222b9c8026750012
baohan <baohanddd@gmail.com>
2016-08-23 14:08:20 +0800

    Added part of autoload

--- example.php
+++ example.php
@@ -2,24 +2,6 @@
 define('APP_PATH', realpath('.'));
 define('DS', "::");
 
-$_route_map = [
-    "baohan\SwooleGearman"  => APP_PATH . "/",
-];
-
-spl_autoload_register(function($class) use ($_route_map) {
-    static $ds = '/';
-
-    $parts  = explode('\\', $class);
-    $app    = array_shift($parts);
-    $module = array_shift($parts);
-    if(!isset($_route_map[$app."\\".$module])) {
-        echo 'Can not found file '.$class;
-        exit();
-    }
-    $path = $_route_map[$app."\\".$module] . str_replace('\\', $ds, implode('\\', $parts)) . '.php';
-    include $path;
-});
-
 try {
     $worker = new \baohan\SwooleGearman\Queue\Worker();
     $worker->addCallback('user::created', function ($payload) {
